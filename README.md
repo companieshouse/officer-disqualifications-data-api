93afcaa358f31d99c762152c329f123bf7c2923f
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2022-03-03 14:06:35 +0000

    Initial skeleton application commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # disqualified-officers-data-api
 Handles CRUD functions for disqualified officers.
+
+# Requirements
+
+- [Java 8](https://www.oracle.com/uk/java/technologies/javase/jdk11-archive-downloads.html)
+- [Maven](https://maven.apache.org/download.cgi)
+- [Git](https://git-scm.com/downloads)
---
+++ src/main/java/uk/gov/companieshouse/disqualifiedofficersdataapi/DisqualifiedOfficersDataApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.disqualifiedofficersdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisqualifiedOfficersDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisqualifiedOfficersDataApiApplication.class, args);
+	}
+
+}
