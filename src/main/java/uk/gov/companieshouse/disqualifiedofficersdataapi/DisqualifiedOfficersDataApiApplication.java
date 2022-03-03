f2486db40c6d1ea469b315fbe8f6476d061d2976
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2022-03-03 14:55:02 +0000

    Formatting correction and amended url path for healthcheck

--- src/main/java/uk/gov/companieshouse/disqualifiedofficersdataapi/DisqualifiedOfficersDataApiApplication.java
+++ src/main/java/uk/gov/companieshouse/disqualifiedofficersdataapi/DisqualifiedOfficersDataApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DisqualifiedOfficersDataApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DisqualifiedOfficersDataApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DisqualifiedOfficersDataApiApplication.class, args);
+    }
 
 }
