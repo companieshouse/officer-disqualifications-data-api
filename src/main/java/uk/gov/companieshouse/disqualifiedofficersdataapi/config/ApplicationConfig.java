1e917fd26eabacaec2eea0d78ae861032b28c508
amartin7663 <almartin@companieshouse.gov.uk>
2022-04-07 17:08:15 +0100

    add localdate serializer for saving dates in mongo

--- src/main/java/uk/gov/companieshouse/disqualifiedofficersdataapi/config/ApplicationConfig.java
+++ src/main/java/uk/gov/companieshouse/disqualifiedofficersdataapi/config/ApplicationConfig.java
@@ -1,32 +1,61 @@
 package uk.gov.companieshouse.disqualifiedofficersdataapi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import uk.gov.companieshouse.api.InternalApiClient;
+import uk.gov.companieshouse.disqualifiedofficersdataapi.converter.DisqualifiedOfficerWriteConverter;
+import uk.gov.companieshouse.disqualifiedofficersdataapi.serialization.LocalDateSerializer;
 import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
+import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
 
-import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.List;
 
 @Configuration
 public class ApplicationConfig {
-    
-    @Autowired
-    private MappingMongoConverter mappingMongoConverter;
 
     @Bean
     EnvironmentReader environmentReader() {
         return new EnvironmentReaderImpl();
     }
 
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public InternalApiClient internalApiClient() {
+        return ApiSdkManager.getPrivateSDK();
+    }
 
     /**
-     * Converter to remove _class from the mongo record during saving
+     * mongoCustomConversions.
+     *
+     * @return MongoCustomConversions.
      */
-    @PostConstruct
-    public void createConverterToRemoveClassName() {
-        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    @Bean
+    public MongoCustomConversions mongoCustomConversions() {
+        ObjectMapper objectMapper = mongoDbObjectMapper();
+        return new MongoCustomConversions(List.of(new DisqualifiedOfficerWriteConverter(objectMapper)));
+    }
+
+    /**
+     * Mongo DB Object Mapper.
+     *
+     * @return ObjectMapper.
+     */
+    private ObjectMapper mongoDbObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(LocalDate.class, new LocalDateSerializer());
+        objectMapper.registerModule(module);
+        return objectMapper;
     }
 }
