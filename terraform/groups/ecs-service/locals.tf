b2220623803413460224f305373c4d88c8c44555
scunnahhyland-ch <scunnahhyland@companieshouse.gov.uk>
2024-05-01 11:15:30 +0100

    Fix: removing duplicate log levels as these are already set in ecs config

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -9,7 +9,7 @@ locals {
   docker_repo                = "disqualified-officers-data-api"
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
   lb_listener_rule_priority  = 57
-  lb_listener_paths          = ["/disqualified-officers/natural/*/internal", "/disqualified-officers/corporate/*/internal", "/disqualified-officers/delete/*/internal", "/disqualified-officers/natural/*", "/disqualified-officers/corporate/*"]
+  lb_listener_paths          = ["/disqualified-officers/natural/*/internal", "/disqualified-officers/corporate/*/internal", "/disqualified-officers/delete/*/internal", "/disqualified-officers/natural/*", "/disqualified-officers/corporate/*", "/disqualified-officers/healthcheck"]
   healthcheck_path           = "/disqualified-officers/healthcheck" #healthcheck path for disqualified officers data api
   healthcheck_matcher        = "200"
   s3_config_bucket           = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
@@ -61,7 +61,9 @@ locals {
   # secrets to go in list
   task_secrets = concat(local.global_secret_list,local.service_secret_list)
 
-  task_environment = concat(local.ssm_global_version_map,local.ssm_service_version_map)
+  task_environment = concat(local.ssm_global_version_map,local.ssm_service_version_map,[
+    { "name" : "PORT", "value" : local.container_port }
+  ])
 
 # get eric secrets from global secrets map
   eric_secrets = [
